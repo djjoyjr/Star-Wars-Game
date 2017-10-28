080f9efcca3f5bdf06524ba3093ff5c743fc93fc
Donald Joy <djsmacbookpro-2017@Donalds-MBP.fios-router.home>
2017-10-28 18:59:13 -0400

    on click registering but not working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,50 +1,52 @@
+var yourCharacter;
+var defender;
+var defender1;
+var count = 0; // KEEPS TRACK OF WINS EACH ROUND..
+
+function setFighterAttributes (fighter, index) {
+
+	fighter.data("charname", characters[index].name);
+	fighter.data("charHP", characters[index].HP);
+	fighter.data("charAP", characters[index].AP);
+	fighter.data("charCAP", characters[index].CAP);
+}
+
+var characters = [
+  { name: "Yoda", HP: 120, AP: 8, CAP: 8, image:"./assets/images/yoda.png"},
+  { name: "Darth Vader", HP: 140, AP: 10, CAP: 10, image:"./assets/images/darth.png"},
+  { name: "Mace Windu", HP: 110, AP: 13, CAP: 13, image:"./assets/images/mace.png"},
+  { name: "Emperor", HP: 130, AP: 5, CAP: 5, image:"./assets/images/emperor.png"}
+ ];
+
 $(document).ready(function() {
 
-var counter = 0;
-var wins = 0;
-var losses = 0;
-var crystalRandomValues = new Array(4);
-var targetNumber = Math.floor(Math.random() * 120) + 19;
-$("#number-to-guess").text(targetNumber);
-for (var i = 0; i < crystalRandomValues.length; i++) {
-  crystalRandomValues[i] = Math.floor(Math.random() * 12)+1;
-}
-for (var i = 0; i < crystalRandomValues.length; i++) {
-
-  var jediPic = $("<img>");
-  jediPic.addClass("jedi-pic");
-  jediPic.attr("src", "./assets/images/jedi" + (i+1) + ".png");
-  $("#your-character").append(jediPic);
-}
-
-$(".gem-pic").on("click", function() {
-  var crystalValue = ($(this).attr("data-crystalvalue"));
-  crystalValue = parseInt(crystalValue);
-  counter += crystalValue;
-    if (counter === targetNumber) {
-    alert("You win!");
-    wins ++;
-    reset();
-  }
-  else if (counter >= targetNumber) {
-    alert("You lose!!");
-    losses ++;
-    reset();
-  }
-  $("#scoreDiv").html("<span>Your current score is:  </span>" + counter);
-  $("#wins").html("<span>Wins:  </span>" + wins);
-  $("#losses").html("<span>Losses:  </span>" + losses);
-});
-function reset() {
-    counter = 0;
-    targetNumber = Math.floor(Math.random() * 120) + 19;
-    $("#number-to-guess").text(targetNumber);
-    for (var i = 0; i < crystalRandomValues.length; i++) {
-      crystalRandomValues[i] = Math.floor(Math.random() * 12)+1;
+  function loadCharacters(){
+  //A LOOP THAT ITERATES THROUGH THE CHARACTERS ARRAY...AND DYNAMICALLY CREATING DIVS FOR ALL PLAYERS..
+    for(var i = 0; i < characters.length; i++) {
+      var charactersDiv = $("<div>").addClass("characters player");
+      setFighterAttributes(charactersDiv, i);
+    //APPENDING THE NEW DIV TO THE TOP ROW "yourCharactersList"....
+      $("#yourCharactersListId").append(charactersDiv);
+        charactersDiv.append("<p>" + characters[i].name + "</p>");
+            charactersDiv.css("background-image", "url(" + characters[i].image + ")");
+            charactersDiv.append("<p>" + characters[i].HP + "</p>");
     }
   }
+  loadCharacters();
 
+  $("#yourCharactersListId").on("click", function() {
+    for (var i = 0; i < characters.length; i++) {
+      if($(this).data("charname") === characters[i].name){
+        var player1 = $("<div>").addClass("col-md-3 characters player");
+        yourCharacter = characters[i];
+        setFighterAttributes (player1, i);
 
-
-
-});
+        $("#yourCharacterId").append(player1);
+        player1.append("<p>" + characters[i].name + "</p>");
+        player1.css("background-image", "url(" + characters[i].image + ")");
+        var player1HP_p = $("<p>").addClass("yourPlayerHP").html(characters[i].HP);
+        player1.append(player1HP_p);
+        }
+    }
+  })
+})
