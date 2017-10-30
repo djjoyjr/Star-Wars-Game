febf439628e465bbb833b43c6394105a8b4a8f05
Donald Joy <djsmacbookpro-2017@Donalds-MBP.fios-router.home>
2017-10-30 17:19:16 -0400

    Final tinkering

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,17 @@
 var characters = [    // this is an object that stores each character's name, stats, and picture
                       //this object is the souce from which jquery will dynamically create
                       //the elements of the game.
-	{ name: "Yoda", HP: 120, AP: 8, CAP: 8, image:"./assets/images/yoda.png"},
-	{ name: "Darth Vader", HP: 140, AP: 10, CAP: 10, image:"./assets/images/darth.png"},
-	{ name: "Mace Windu", HP: 110, AP: 13, CAP: 13, image:"./assets/images/mace.png"},
-	{ name: "Emperor", HP: 130, AP: 5, CAP: 5, image:"./assets/images/emperor.png"}
+	{ name: "Yoda", HP: 80, AP: 11, CAP: 15, image:"./assets/images/yoda.png"},
+	{ name: "Darth Vader", HP: 150, AP: 12, CAP: 9, image:"./assets/images/darth.png"},
+	{ name: "Mace Windu", HP: 120, AP: 8, CAP: 13, image:"./assets/images/mace.png"},
+	{ name: "Emperor", HP: 100, AP: 13, CAP: 8, image:"./assets/images/emperor.png"}
  ];
 var yourCharacter;  //global variable assigned to the character you choose to play with
 var defender;      //global variable assigned to the first defender you select
 var defender1;     //global variable assigned to the second defender you select
 var count = 0; // tracks how many rounds you've won
 var enemiesAvailable;
-
+var currentAP;
 
 //this function accepts two values: fighter and index then populates fighter with the
 //data from the object above the index argument allows us to accept an iterator in
@@ -29,9 +29,7 @@ function setFighterAttributes (fighter, index) {
 //this function will dynamically populate the empty div #yourCharactersListId with the four character
 //choices.  We will call it to render the initial game board
 function loadCharacters(){
-
-	$("#yourCharactersListId").text("Available Characters:");
-
+		$("#yourCharactersListId").text("Available Characters:");
 
 		//iterates throuth the characters[] array, and dynamically creates a div for each character
     //using the setFighterAttributes function above
@@ -56,8 +54,6 @@ $(document).ready(function() {
 //is actually rendered to the DOM
 	loadCharacters();
 
-
-
 	//this listening for any click on something with a class of .player (which all of our character
 //divs have as a result of line 35 above)
 	$(".player").on("click", function() {
@@ -71,8 +67,7 @@ $(document).ready(function() {
 				var player1 = $("<div>").addClass("col-md-3 characters player");
 				yourCharacter = characters[i];
 				setFighterAttributes(player1, i);
-
-
+				currentAP=yourCharacter["AP"];
 
 				//still inside the conditional, this block of code appends everthing that happened in 63-66
         //into the previously empty div with the id #yourCharacterId
@@ -102,18 +97,16 @@ $(document).ready(function() {
 			//ATTACHING ON-CLICK EVENTS TO "yourDefenderId" DIV....
 			$(".player").on("click", function(){
 
-				$(yourDefenderId).text("Your Adversary:");
-
+				$("#yourDefenderId").text("Your Adversary:");
 
 				for(var i = 0; i < enemiesAvailable.length; i++) {
 
 					if($(this).data("charname") === characters[i].name){
-			  			//ASSINGING A NEW DIV FOR IT..
 						defender1 = $("<div>").addClass("col-md-3 characters defender");
 						defender = characters[i];
 						setFighterAttributes(defender1, i);
 
-						//APPENDING THE CHOSEN PLAYER TO THE "yourCharcterId" DIV....
+						//Appending the chosen defender to the yourDefenderId div
 						$("#yourDefenderId").append(defender1);
 						defender1.append("<p>" + characters[i].name + "</p>");
 						defender1.css("background-image", "url(" + characters[i].image + ")");
@@ -132,32 +125,43 @@ $(document).ready(function() {
 		}
 
 		pickDefender();
-
 		//DYNAMICALLY CREATING THE ATTACK BUTTON......
 		var attack = $("<button>").addClass("btn-danger").html("Attack ").appendTo("#clickHere");
 
 		//ATTACK BUTTON ONCLICK EVENT HANDLER..
 		$("#clickHere").on("click", function() {
 
-			// sounds.attack.sound.play();
+			var audioElement = document.createElement("audio");
+			var j = [Math.floor(Math.random()*4)];
+			audioElement.setAttribute("src", "./assets/ls" + [j] +".wav");
+				audioElement.play();
+
+
 			startAttack();
-			if (count === 2) {
+			if (count === 2 && defender["HP"] <= 0) {
 
 				$(".btn-danger").prop('disabled', true);
-		 		$(".message").html("Congratulations!!! You WON the game!");
-        // nds.win.sound.play();
+				audioElement.setAttribute("src", "./assets/win.wav");
+					audioElement.play();
+				alert("Congratulations, Jedi.  May the Force be with you.")
+		 		$(".message").html("Congratulations!!! You WON the game! Refresh your browser to play again.");
+				$("#combat-updates").text("");
 
 			 } else if (yourCharacter["HP"] <= 0){
 					$(".btn-danger").prop('disabled', true);
-					$(".message").text("You have been defeated, play again?");
-					// sounds.lose.sound.play();
+					audioElement.setAttribute("src", "./assets/lose.wav");
+					audioElement.play();
+					alert("Fear is the path to the dark side. Fear leads to anger. Anger leads to hate. Hate leads to suffering.");
+					$(".message").text("You have been defeated, refresh your browser to play again.");
+					$("#combat-updates").text("");
+
 
 				} else if (defender["HP"] <= 0){
 
 					$(".btn-danger").prop('disabled', true);
 					$(".message").text("You won this round. Please select your next Defender");
 					count++;
-					// sounds.win.sound.play();
+					$("#yourDefenderId").html(" ");
 					defender1.hide();
 
 					pickDefender();
@@ -167,13 +171,16 @@ $(document).ready(function() {
 		});
 
 		function startAttack() {
-
-			yourCharacter["HP"] -= defender["AP"];
+			$("#combat-updates").text(yourCharacter["name"] + " attacked " + defender ["name"] + " for " + currentAP + " points.");
+			
+			yourCharacter["HP"] -= defender["CAP"];
 			$(".yourPlayerHP").text("HP: " + yourCharacter["HP"]);
 
-			defender["HP"] -= yourCharacter["AP"];
+			defender["HP"] -= currentAP;
 			$(".yourDefenderHP").text("HP: " + defender["HP"]);
-			yourCharacter["AP"] += yourCharacter["CAP"];
+			currentAP += yourCharacter["AP"];
+
+
 		}
 	});
 });
